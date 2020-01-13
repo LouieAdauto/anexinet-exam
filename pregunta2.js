7849c54b629cb6584cf26c6ffb7142961a3d654b
LouieAdauto <fernandoadauto.c@gmail.com>
2020-01-12 21:41:36 -0600

    finalizado ejercicio de binarios

--- pregunta2.js
+++ pregunta2.js
@@ -1,23 +1,33 @@
 //print binary of a number between 0 and 1
 let binarray = [];
+let numeroEnString = "";
+let floatEnArr = [];
 
-const intToBin = (number) => {    
-    if (number === 1){
-        return binarray.push(number);
-    }else {   
-        if(number%2 === 0){
-              binarray.push(0)  
-        }else{
-            binarray.push(1)
-        }        
-        return intToBin(
-            parseInt(number/2)
-        )
-        
-    }
+const intToBin = (numero, arr, index) => {
+            numero = numero * 2; 
+            while(arr.length < 32){
+                numeroEnString = numero+"";
+                if(numeroEnString.charAt(0)==1){
+                    binarray.push(1)
+                    floatEnArr = numeroEnString.split("")                    
+                    floatEnArr[0] = "0"
+                    let numeross = arr.join("")
+                    numero = parseFloat(numeross)
+                }else{
+                    if(numeroEnString.charAt(0)==0){
+                        binarray.push(0)
+                        floatEnArr = numeroEnString.split("")                                            
+                        let numeross = arr.join("")
+                        numero = parseFloat(numeross)
+                    }
+                }                
+                index++;
+                intToBin(numero, arr, index)
+            }
+    
 }
-intToBin(15); 
-binarray.length > 30 ? console.log("Error"):console.log(binarray.reverse().join(""));
+intToBin(0.252, binarray, 30); 
+console.log(binarray.reverse().join(""));
 //There was not enough time when I realised it was actually float which the method is to multiply by two till it becomes 1 otherwise is 0
 //But I realised about it too late
 
