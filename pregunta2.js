f26e90ee3161a84bd471e5f4525b652d5b7e3f88
LouieAdauto <fernandoadauto.c@gmail.com>
2020-01-12 21:46:18 -0600

    commit again to change the name of the files

--- pregunta2.js
+++ pregunta2.js
@@ -1,33 +1,26 @@
 //print binary of a number between 0 and 1
-let binarray = [];
-let numeroEnString = "";
-let floatEnArr = [];
-
-const intToBin = (numero, arr, index) => {
-            numero = numero * 2; 
-            while(arr.length < 32){
-                numeroEnString = numero+"";
-                if(numeroEnString.charAt(0)==1){
-                    binarray.push(1)
-                    floatEnArr = numeroEnString.split("")                    
-                    floatEnArr[0] = "0"
-                    let numeross = arr.join("")
-                    numero = parseFloat(numeross)
-                }else{
-                    if(numeroEnString.charAt(0)==0){
-                        binarray.push(0)
-                        floatEnArr = numeroEnString.split("")                                            
-                        let numeross = arr.join("")
-                        numero = parseFloat(numeross)
-                    }
-                }                
-                index++;
-                intToBin(numero, arr, index)
-            }
+let numero = 0.15;
+let arr = [];
+let arraydebinario = [];
+let numeross;
+console.log(numero)
+for (let i = 0; i < 19; i++) {
+    numero = numero * 2;
+    console.log(numero)
+    let numeros = numero+"";
+    if(numeros.charAt(0)==1){        
+        arr = numeros.split("")        
+        arr[0] = "0"
+        arraydebinario.push(1)
+        numeross = arr.join("")
+        numero = parseFloat(numeross)
+    }else{
+        arraydebinario.push(0);
+    }
     
 }
-intToBin(0.252, binarray, 30); 
-console.log(binarray.reverse().join(""));
+console.log(numero)
+console.log(arraydebinario.join(""))
 //There was not enough time when I realised it was actually float which the method is to multiply by two till it becomes 1 otherwise is 0
 //But I realised about it too late
 //I just realised that if you don't set a limit to the calls, the decimals will never stop going
